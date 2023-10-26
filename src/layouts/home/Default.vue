61d663b2fe281fa9163ffe793bf2db4abcafdf4a
DiobarBaez <diobar.baez@uabc.edu.mx>
2023-10-25 22:20:51 -0700

    first commit

---
+++ src/layouts/home/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <home-bar @toggleDrawer="toggleDrawer" />
+  </v-app>
+</template>
+
+<script setup>
+    import HomeBar from './AppBar.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/login/Default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="6">
+          <default-view />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+import DefaultView from './View.vue';
+</script>
